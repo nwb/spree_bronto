8fc2c85b0fa9da04e7aaa366157caa724ce6a436
Albert Liu <albertliu@naturalwellbeing.com>
2016-01-13 20:39:11 -0800

    account email for create added

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -15,7 +15,7 @@ module Spree
       #trigger the email directly here.
       store_code= current_store.code
       token= Spree::BrontoConfiguration.account[store_code]['token']
-      from_email= Spree::BrontoConfiguration.account[store_code][store_code]['from_address']
+      from_email= Spree::BrontoConfiguration.account[store_code]['from_address']
       from_name= Spree::BrontoConfiguration.account[store_code]['from_name']
       reply_email= Spree::BrontoConfiguration.account[store_code]['from_address']
       email_options={:fromEmail =>from_email,:fromName => from_name, :replyEmail => reply_email}
@@ -27,6 +27,7 @@ module Spree
       attributes = {:First_Name => user_order.ship_address.firstname} unless !user_order
       attributes = {:First_Name => user_order.ship_address.lastname} unless !user_order
 
+      attributes||={}
       attributes[:SENDTIME__CONTENT1] = reset_password_token
       attributes[:SENDTIME__CONTENT2] = reset_password_token
 
@@ -46,6 +47,40 @@ module Spree
 
 
     end
+
+    def deliver_confirmation_instructions(current_store)
+
+      store_code= current_store.code
+      token= Spree::BrontoConfiguration.account[store_code]['token']
+      from_email= Spree::BrontoConfiguration.account[store_code]['from_address']
+      from_name= Spree::BrontoConfiguration.account[store_code]['from_name']
+      reply_email= Spree::BrontoConfiguration.account[store_code]['from_address']
+      email_options={:fromEmail =>from_email,:fromName => from_name, :replyEmail => reply_email}
+
+      message_name= Spree::BrontoConfiguration.account[current_store.code]['new_account']
+      email=self.email
+
+      user_order=orders.select{|o| o.state='complete'}.last
+      attributes = {:First_Name => "Customer"}
+      attributes[:First_Name] = user_order.ship_address.firstname unless !user_order
+      attributes[:emailaddr] = self.email
+
+
+      begin
+        communication = BrontoIntegration::Communication.new(token)
+        communication.trigger_delivery_by_id(message_name,email,'transactional','html',attributes,email_options)
+
+      rescue => exception
+        begin    #handle the transactional contact in case the message is not approved for transactional.
+          contact = BrontoIntegration::Contact.new(token)
+          contact.update_status(email,'active')
+          communication.trigger_delivery_by_id(message_name,email,'triggered','html',attributes,email_options)
+        rescue => exception
+          raise exception
+        end
+      end
+    end
+
 
   end
 end

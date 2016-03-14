49a15d971f505bd3fd73544ef4e9c19191bb8951
Albert Liu <albertliu@naturalwellbeing.com>
2016-03-14 15:59:50 -0700

    add ordermailer overwrite

---
+++ app/models/spree/order_mailer_decorator.rb
@@ -0,0 +1,15 @@
+Spree::OrderMailer.class_eval do
+  def confirm_email(order, resend = false)
+    @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+
+    store=@order.store
+    external_key = Spree::BrontoConfiguration.account[store.code]["order_received"]
+    Delayed::Job.enqueue(DelayedSend.new( store.code,
+                                          @order.email,
+                                          external_key,
+                                          @order.id.to_s,
+                                          "order_mailer/order_confirm_plain",
+                                          "order_mailer/order_confirm_html"), {priority: 50})
+
+  end
+end
